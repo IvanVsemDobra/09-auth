850819b9e0163c40eb8f8fb1812a7e67983d9347
Jeka <gekanefed22.04.2010@gmail.com>
2025-11-16 23:45:17 +0300

    ttt

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -47,20 +47,17 @@ export type RegisterRequest = {
 
 export const register = async (data: RegisterRequest) => {
   const res = await nextServer.post<User>("/auth/register", data);
-  
   return res.data;
 };
 
-export interface LoginRequest {
+export type LoginRequest = {
   email: string;
   password: string;
 };
 
 export const login = async (data: LoginRequest) => {
   const res = await nextServer.post<User>("/auth/login", data);
-  
   return res.data;
-  
 };
 
 interface CheckSessionRequest {
